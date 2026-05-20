d9af793b230a163ca7b728560503e91406033f3f
David Ekeocha <davidekeocha36@gmail.com>
2026-05-20 15:25:32 +0100

    Set up initial Pomodoro timer interface

--- 08_pomodoro_timer/main.py
+++ 08_pomodoro_timer/main.py
@@ -11,7 +11,7 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
@@ -20,11 +20,17 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Timer Label
+timer = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer.grid(column=1, row=0)
+
+# Canvas
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
 
 
 window.mainloop()
