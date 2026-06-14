8c339f2c043392cfce9accf699f287a6765b57ae
David Ekeocha <davidekeocha36@gmail.com>
2026-06-14 10:17:09 +0100

    Create flash card layout and display card image

--- 10_flash_card_project/main.py
+++ 10_flash_card_project/main.py
@@ -1,2 +1,20 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+# Window
+window = Tk()
+window.title("Flash Card")
+window.config(padx=50, pady=50)
+
+# Canvas
+canvas = Canvas(width=800, height=526)
+front_image = PhotoImage(file="images/card_front.png")
+back_image = PhotoImage(file="images/card_back.png")
+canvas.create_image(400, 263, image=front_image)
+canvas.grid(row=0, column=1)
+
+window.mainloop()
 
