87acb46ec4d4a3335ae8c50a235d0ce0f0e42289
David Ekeocha <davidekeocha36@gmail.com>
2026-06-23 22:58:18 +0100

    Add flash card navigation buttons and timer cancellation

--- 10_flash_card_project/main.py
+++ 10_flash_card_project/main.py
@@ -4,25 +4,37 @@ import pandas
 
 data = pandas.read_csv("data/french_words.csv")
 
-random_index = random.randint(0, len(data) - 1)
-current_card = data.iloc[random_index]
+current_card = data.iloc[random.randint(0, len(data) - 1)]
+
+flip_timer = None
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-# ---------------------------- FLIP CARD ------------------------------- #
+# ---------------------------- CARD CONFIGURATION ------------------------------- #
 def flip_card():
     canvas.itemconfig(card_background, image=back_image)
     canvas.itemconfig(language_title,text="English")
     canvas.itemconfig(card_word,text=current_card["English"])
 
+def next_card():
+    global current_card, flip_timer
 
+    if flip_timer:
+        window.after_cancel(flip_timer)
+
+    current_card = data.iloc[random.randint(0, len(data) - 1)]
+
+    canvas.itemconfig(card_background, image=front_image)
+    canvas.itemconfig(language_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+    flip_timer = window.after(3000, flip_card)
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 # Window
 window = Tk()
 window.title("Flash Card")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
 # Canvas
 canvas = Canvas(width=800, height=526)
@@ -34,8 +46,16 @@ language_title = canvas.create_text(400, 150, text="French", font=("Arial", 40, 
 card_word = canvas.create_text(400, 263, text=current_card["French"], font=("Arial", 60, "bold"))
 canvas.grid(row=0, column=1)
 
-window.after(3000, flip_card)
+right_image = PhotoImage(file="images/right.png")
+wrong_image = PhotoImage(file="images/wrong.png")
 
+wrong_button = Button(image=wrong_image, command=next_card, highlightthickness=0, bd=0)
+right_button = Button(image=right_image, command=next_card, highlightthickness=0, bd=0)
+
+wrong_button.grid(row=1, column=0)
+right_button.grid(row=1, column=2)
+
+next_card()
 
 window.mainloop()
 
